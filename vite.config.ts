b63329b4e5e4c61357055b8b7148ff6a59bfb673
Young-klynique <appiahyoung2002@gmail.com>
2025-07-15 11:40:42 +0000

    updated the package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ✅ Add this base property
+  base: "/asiedu-nana-portfolio-site/",
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
